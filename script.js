0b8cb8188b31d80f10fdf7f1b2de89fe943e8abb
PRIYANKA <priyankagariya18@gmail.com>
2024-07-31 10:33:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+let btnLinks = document.querySelectorAll('p div a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop ;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+
+        }
+    });
+}
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop ;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            btnLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('p div  a [href*=' + id + ']').classList.add('active');
+            });
+
+        }
+    });
+}
